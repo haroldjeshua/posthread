f4edb766a0dd38aa437d76e2022e22917d9f2783
Harold Varde <harvardii15@gmail.com>
2023-07-08 23:14:45 +0800

    threadform

--- src/components/NewThreadForm.tsx
+++ src/components/NewThreadForm.tsx
@@ -1,19 +1,26 @@
+import { useState } from "react";
 import { useSession } from "next-auth/react";
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
 
 export default function NewThreadForm() {
-    const session = useSession()
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
 
-    if (session.status !== "authenticated") return
+  if (session.status !== "authenticated") return;
 
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
-        <div className="flex items-center gap-4">
-            <ProfileImage src={session.data.user.image} />
-            <textarea className="flex-frow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="Wanna share something?" />
-        </div>
-        <Button className="self-end">Post</Button>
+      <div className="flex items-center gap-4">
+        <ProfileImage src={session.data.user.image} />
+        <textarea
+          style={{ height: 0 }}
+          value={inputValue}
+          className="flex-frow resize-none overflow-hidden p-4 text-lg outline-none"
+          placeholder="Wanna share something?"
+        />
+      </div>
+      <Button className="self-end">Post</Button>
     </form>
-  )
+  );
 }
